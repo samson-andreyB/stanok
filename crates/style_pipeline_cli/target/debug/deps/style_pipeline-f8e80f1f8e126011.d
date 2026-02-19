83dc6e6e0f73c47c1f80427c02189ac33e9b0104
Белявский Андрей <belyavskiy@samsonpost.ru>
2026-02-19 14:28:09 +0300

    chore(style-pipeline-cli): remove generated artifacts from repo

--- crates/style_pipeline_cli/target/debug/deps/style_pipeline-f8e80f1f8e126011.d
+++
@@ -1,7 +0,0 @@
-/home/snow/App/stanok-test/crates/style_pipeline_cli/target/debug/deps/style_pipeline-f8e80f1f8e126011.d: /home/snow/App/stanok-test/crates/style_pipeline/src/lib.rs
-
-/home/snow/App/stanok-test/crates/style_pipeline_cli/target/debug/deps/libstyle_pipeline-f8e80f1f8e126011.rlib: /home/snow/App/stanok-test/crates/style_pipeline/src/lib.rs
-
-/home/snow/App/stanok-test/crates/style_pipeline_cli/target/debug/deps/libstyle_pipeline-f8e80f1f8e126011.rmeta: /home/snow/App/stanok-test/crates/style_pipeline/src/lib.rs
-
-/home/snow/App/stanok-test/crates/style_pipeline/src/lib.rs:
