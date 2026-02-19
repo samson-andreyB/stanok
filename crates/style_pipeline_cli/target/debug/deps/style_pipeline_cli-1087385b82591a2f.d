dd28a7cf07c8126bd489322e257193ffffb2704c
Белявский Андрей <belyavskiy@samsonpost.ru>
2026-02-19 14:28:01 +0300

    feat(style-pipeline-cli): add compile command skeleton and parser tests

---
+++ crates/style_pipeline_cli/target/debug/deps/style_pipeline_cli-1087385b82591a2f.d
@@ -0,0 +1,5 @@
+/home/snow/App/stanok-test/crates/style_pipeline_cli/target/debug/deps/style_pipeline_cli-1087385b82591a2f.d: src/main.rs
+
+/home/snow/App/stanok-test/crates/style_pipeline_cli/target/debug/deps/style_pipeline_cli-1087385b82591a2f: src/main.rs
+
+src/main.rs:
